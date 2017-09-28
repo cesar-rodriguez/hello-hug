e03088d9d7e71e2ee807e7fa959d547bdcf187a1
Cesar Rodriguez <therasec@gmail.com>
2017-09-28 18:37:01 -0400

    fixes 0.0.0.0/0

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = "80"
     to_port     = "80"
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
 }
 
