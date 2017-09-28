7ab86fa86a500940233932720977964927faddee
Cesar Rodriguez <therasec@gmail.com>
2017-09-28 18:30:31 -0400

    formats

--- main.tf
+++ main.tf
@@ -18,13 +18,14 @@ provider "aws" {
 }
 
 resource "aws_security_group" "web_sg" {
-  name = "web"
+  name        = "web"
   description = "Security group for web server"
-  vpc_id = "${var.vpc_id}"
+  vpc_id      = "${var.vpc_id}"
+
   ingress {
-    from_port = "80"
-    to_port = "80"
-    protocol = "tcp"
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
