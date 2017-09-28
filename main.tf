be59a6a6ce9363175ef8ef3ca35afb49468adb21
Cesar Rodriguez <therasec@gmail.com>
2017-09-28 18:25:12 -0400

    changes sg name

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "web_sg" {
-  name = "web-sg"
+  name = "web"
   description = "Security group for web server"
   vpc_id = "${var.vpc_id}"
   ingress {
